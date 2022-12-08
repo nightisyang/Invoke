909458357dab65dee69225ebd2e15dfbf0994588
nightisyang <shenyangc86@gmail.com>
2022-12-08 15:28:02 +0800

    init barebones

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { useState } from "react";
+import "./styles.css";
+
+const App = () => {
+  const [firstNumber, setFirstNumber] = useState();
+
+  function generateFirstNumber() {
+    let firstNumber = Math.floor(Math.random() * 9) + 1;
+    console.log(firstNumber);
+    setFirstNumber(firstNumber);
+  }
+
+  return (
+    <div className="container">
+      <div className="split">
+        <div className="window">
+          <h1>Summation operation of a series of random numbers</h1>
+          <p>Create a web page with ReactJS.</p>
+          <p>
+            A “Start” button for users to trigger a new operation where a series
+            of random numbers between 1 to 9 will be generated.
+          </p>
+          <p>
+            The first number generated will determine the number of subsequent
+            digits to be needed for the sum operation.
+          </p>
+          <p>
+            The output on the page will display the numbers generated and the
+            result of the summation.
+          </p>
+          <p>Example 1:</p>
+          <p>1st number : 4</p>
+          <p>Next four numbers generated : 6, 7, 2, 5</p>
+          <p>666666 + 7777777 + 22 + 55555 = 8500020</p>
+          <p>Example 2:</p>
+          <p> 1st number : 6</p>
+          <p>
+            Next six numbers generated : 3, 2, 1, 7, 8, 2 333 + 22 + 1 + 7777777
+            + 88888888 + 22 = 96667043{" "}
+          </p>
+          <p>Submission instructions: </p>
+          <p>
+            1. Attach a brief description of your code design approach in terms
+            of efficiency and maintainability considerations.{" "}
+          </p>
+          <p>
+            2. Provide details of testing (with test cases) performed on your
+            program.
+          </p>
+          <p>
+            3. Also include the references (eg. online articles, videos) you
+            have accessed to complete the test.
+          </p>
+          <p>
+            4. Deploy the web page and send us the URL to access the live site.
+          </p>
+          <p>
+            5. Upload the code to git and send us the URL to access the repo.
+            Deadline: 5 days
+          </p>
+        </div>
+        <div className="test">
+          <h1>Test</h1>
+          <span>
+            {" "}
+            <button onClick={generateFirstNumber}>
+              Generate First Number
+            </button>{" "}
+            First number generated: {firstNumber}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
